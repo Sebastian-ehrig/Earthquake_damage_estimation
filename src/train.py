d80adc1ba1799fcb9b0125ca5d5b5a2c96e91506
LuiseStrathe <101968059+LuiseStrathe@users.noreply.github.com>
2022-04-28 16:47:40 +0200

    profiler report & train structure

--- src/train.py
+++ src/train.py
@@ -1,11 +1,19 @@
 from helper.conf import *
 from functions.helper import load_data
+import pandas as pd
 
 
+# load data
 train_values = load_data(TRAIN_VALUES_PATH)
 train_labels = load_data(TRAIN_LABELS_PATH)
+test_values = load_data(TEST_VALUES_PATH)
+
+# combine values of train and test set 
+X_all_raw = pd.concat([train_values, test_values], axis=0, sort=False)
 
 
+# make report on values
 from pandas_profiling import ProfileReport
-prof = ProfileReport(train_values)
-prof.to_file(output_file='./reports/output.html')
+prof = ProfileReport(X_all_raw)
+prof.to_file(output_file='./reports/data_report.html')
+
