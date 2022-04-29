d76285e391de929fafe408dabac9206be8bc2fe6
sebastian-ehrig <sebast.ehrig@gmail.com>
2022-04-29 10:33:20 +0200

    changed data types

--- src/functions/helper.py
+++ src/functions/helper.py
@@ -5,3 +5,10 @@ import pandas as pd
 def load_data(path):
     df = pd.read_csv(path)
     return df
+
+def change_datatype(data, string_columns, int_columns):
+    data.loc[:, string_columns] = data.loc[:, string_columns].astype(str)
+    data.loc[:, int_columns] = data.loc[:, int_columns].astype(int)
+    return data
+
+        
--- src/train.py
+++ src/train.py
@@ -1,16 +1,31 @@
 
 from functions.train_model import train_model
+from helper.conf import *
+from functions.helper import change_datatype
+from functions.helper import load_data
+import pandas as pd
 
+# LOAD DATA
+train_values = load_data(TRAIN_VALUES_PATH)
+train_labels = load_data(TRAIN_LABELS_PATH)
+test_values = load_data(TEST_VALUES_PATH)
 
-# fitting the data
-fitted_model = train_model(train_values, train_labels, model)
-print("Model is trained")
+## combine values of train and test set 
+X_all_raw = pd.concat([train_values, test_values], axis=0, sort=False)
 
-# calculate f1-score on training data (not test score, incl. validation & training data)
-f1score = model.predict(X_train)
+X_all_shape = change_datatype(X_all_raw, string_columns, int_columns)
 
-print(f"The score on the training data is {f1score}")
+a = 1
 
-# predict on test set
-prediction = model.predict(X_test)
-print("Predictions on test data are done")
+# # fitting the data
+# fitted_model = train_model(train_values, train_labels, model)
+# print("Model is trained")
+
+# # calculate f1-score on training data (not test score, incl. validation & training data)
+# f1score = model.predict(X_train)
+
+# print(f"The score on the training data is {f1score}")
+
+# # predict on test set
+# prediction = model.predict(X_test)
+# print("Predictions on test data are done")
